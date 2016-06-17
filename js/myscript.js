64f029e4e59fc0c992d762178ca05e17509b162d
Heijira <vince.feger@gmail.com>
2016-06-17 15:15:53 +0200

    portfolio0.8

--- js/myscript.js
+++ js/myscript.js
@@ -10,22 +10,16 @@
     // });
 
 
-    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+    /* Open the sidenav */
     function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
+        document.getElementById("mySidenav").style.width = "100%";
     }
 
-    // function openNav(){
-    //   $("#mySidenav").css().width("250px");
-    //   $("#mySidenav").css().marginLeft("250px");
-    // }
-
-    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+    /* Close/hide the sidenav */
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
     }
+
 
   $(document).ready(function() {
 		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
