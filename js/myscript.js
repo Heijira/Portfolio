09caf8391f45e31ab0114b73936b4b6945efcc5b
Heijira <vince.feger@gmail.com>
2016-06-17 09:18:34 +0200

    test

--- js/myscript.js
+++ js/myscript.js
@@ -10,6 +10,23 @@
     // });
 
 
+    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+
+    // function openNav(){
+    //   $("#mySidenav").css().width("250px");
+    //   $("#mySidenav").css().marginLeft("250px");
+    // }
+
+    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
+
   $(document).ready(function() {
 		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
 			var page = $(this).attr('href'); // Page cible
