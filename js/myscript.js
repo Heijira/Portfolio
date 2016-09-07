98e83a45a608457dd2ca86ba5b3b622a24f0d1cc
Heijira <vince.feger@gmail.com>
2016-09-07 15:15:33 +0200

    Portfolio 0.95

--- js/myscript.js
+++ js/myscript.js
@@ -1,25 +1,16 @@
-    //
-    //
-    // $(document).ready(function() {
-    //   $("#tronche").scroll(function(event) {
-    //     var page = $(this).attr('href'); // Page cible
-    //     var speed = 1000; // Durée de l'animation (en ms)
-    //     $('html, body').animate( { scrollTop: $(page).offset().top }, speed );
-    //     event.preventDefault(); // Go
-    //   });
-    // });
 
-    /* Open the sidenav */
+    /* Ouvrir le sidenav */
     function openNav() {
         document.getElementById("mySidenav").style.width = "100%";
     }
 
-    /* Close/hide the sidenav */
+    /* Fermer le sidenav */
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
     }
 
 
+    /* Fonction pour le scroll au clic */
   $(document).ready(function() {
 		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
 			var page = $(this).attr('href'); // Page cible
@@ -30,6 +21,42 @@
 	});
 
 
-  $(document).ready(function ($) {
-      $(window).stellar();
-  });
+    // formulaire !!
+      $(function(){
+          $("#contact").submit(function(event){
+              var nom        = $("#nom").val();
+              var sujet      = $("#sujet").val();
+              var email      = $("#email").val();
+              var message    = $("#message").val();
+              var dataString = nom + sujet + email + message;
+              var msg_all    = "Merci de remplir tous les champs ;)";
+              var msg_alert  = "Merci de remplir ce champs ;)";
+
+              if (dataString  == "") {
+                  $("#msg_all").html(msg_all);
+              } else if (nom == "") {
+                  $("#msg_nom").html(msg_alert);
+              } else if (sujet == "") {
+                  $("#msg_sujet").html(msg_alert);
+              } else if (email == "") {
+                  $("#msg_email").html(msg_alert);
+              } else if (message == "") {
+                  $("#msg_message").html(msg_alert);
+              } else {
+                  $.ajax({
+                      type : "POST",
+                      url: $(this).attr("action"),
+                      data: $(this).serialize(),
+                      success : function() {
+                          $("#contact").html("<h3>Le message a bien été envoyé :)</h3>");
+                      },
+                      error: function(e) {
+
+                          $("#contact").html("<h3>Le message a bien été envoyé :)</h3>"+e.getMessage());
+                      }
+                  });
+              }
+
+              return false;
+          });
+      });
